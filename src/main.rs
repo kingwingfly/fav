59f7cba2354f7a2921efb3225a59f562e1d51e26
Louis <836250617@qq.com>
2024-01-25 03:00:14 +0800

    clap, tracing

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,35 @@
+use clap::{Parser, Subcommand};
+use tracing::info;
+
+#[derive(Parser)]
+#[command(author, version, about)]
+struct Cli {
+    #[clap(subcommand)]
+    subcmd: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    Init { path: Option<std::path::PathBuf> },
+    Fetch {},
+}
+
 fn main() {
-    println!("Hello, world!");
+    tracing_subscriber::fmt()
+        .without_time()
+        .with_target(false)
+        .with_max_level(tracing::Level::DEBUG)
+        .init();
+
+    let args = Cli::parse();
+    match args.subcmd {
+        Commands::Init { path } => {
+            let path = path
+                .unwrap_or(std::path::PathBuf::from("."))
+                .join(".backup");
+            info!("init {}", path.display());
+            std::fs::create_dir_all(&path).unwrap();
+        }
+        Commands::Fetch {} => todo!(),
+    }
 }
