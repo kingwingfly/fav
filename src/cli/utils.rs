9c98d5828ff49ca3c2b86c88db1475d379fc163d
Louis <836250617@qq.com>
2024-01-26 23:07:53 +0800

    status show total

--- src/cli/utils.rs
+++ src/cli/utils.rs
@@ -13,11 +13,15 @@ where
 {
     let mut builder = Builder::default();
     builder.push_record(header);
-    rows.into_iter().for_each(|r| builder.push_record(r));
+    let mut count = 0;
+    rows.into_iter()
+        .inspect(|_| count += 1)
+        .for_each(|r| builder.push_record(r));
     let table = builder
         .build()
         .with(Style::modern())
         .modify(Rows::new(1..), Alignment::left())
         .to_string();
     println!("{}", table);
+    println!("Total: {}", count);
 }
