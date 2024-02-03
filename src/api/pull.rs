00bef3adfe99730f685fea79222c87d27575d317
Louis <836250617@qq.com>
2024-02-03 14:02:02 +0800

    simplify pull

--- src/api/pull.rs
+++ src/api/pull.rs
@@ -8,24 +8,6 @@ use tracing::warn;
 
 const API: &str = "https://api.bilibili.com/x/player/playurl";
 
-struct PullOption {
-    title: String,
-    bvid: String,
-    cid: i64,
-    clarity: Clarity,
-}
-
-impl From<VideoMeta> for PullOption {
-    fn from(value: VideoMeta) -> Self {
-        Self {
-            title: value.title,
-            bvid: value.bvid,
-            cid: value.cid,
-            clarity: value.clarity.unwrap(),
-        }
-    }
-}
-
 pub(crate) async fn pull_all() {
     let videos = meta().videos.iter().filter(|v| v.track).collect();
     try_pull(videos).await;
@@ -43,13 +25,13 @@ pub(crate) async fn pull(id: Vec<String>) {
     try_pull(videos).await;
 }
 
-async fn try_pull(videos: Vec<&VideoMeta>) {
+async fn try_pull(videos: Vec<&'static VideoMeta>) {
     let mut meta = meta().clone();
     for batch in videos.chunks(10) {
         let jhs: Vec<_> = batch
-            .iter()
+            .into_iter()
             .filter(|v| v.track && !v.saved)
-            .map(|&v| tokio::spawn(do_pull(v.clone().into())))
+            .map(|&v| tokio::spawn(do_pull(v)))
             .collect();
         for jh in jhs {
             match jh.await.unwrap() {
@@ -67,13 +49,13 @@ async fn try_pull(videos: Vec<&VideoMeta>) {
     meta.persist();
 }
 
-async fn do_pull(opt: PullOption) -> Result<String> {
+async fn do_pull(opt: &VideoMeta) -> Result<String> {
     let url = reqwest::Url::parse_with_params(
         API,
         [
             ("bvid", opt.bvid.as_str()),
             ("cid", &opt.cid.to_string()),
-            ("qn", &opt.clarity.to_qn()),
+            ("qn", &opt.clarity.unwrap().to_qn()),
         ],
     )
     .unwrap();
@@ -82,9 +64,9 @@ async fn do_pull(opt: PullOption) -> Result<String> {
     match json["code"].as_i64().unwrap() {
         0 => {
             let mut play_info: PlayInfo = parse_message(&json["data"]["durl"][0]);
-            play_info.title = opt.title;
+            play_info.title = opt.title.to_owned();
             download(play_info).await?;
-            Ok(opt.bvid)
+            Ok(opt.bvid.to_owned())
         }
         _ => PullFail {
             msg: format!("Pull Fail bvid:{}; Expired or other reason.", opt.bvid),
@@ -140,21 +122,5 @@ impl Clarity {
             Clarity::VLD => "6",
         }
         .to_string()
-    }
-}
-
-#[cfg(test)]
-mod tests {
-    use super::*;
-
-    #[tokio::test]
-    async fn pull_test() {
-        let opt = PullOption {
-            title: "test".to_string(),
-            bvid: "BV157411h7Et".to_string(),
-            cid: 148866581,
-            clarity: Clarity::VLD,
-        };
-        do_pull(opt).await.unwrap();
     }
 }
