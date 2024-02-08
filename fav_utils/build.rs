7d1eeefe9e879ec7e17b54e42ae933218b3c5277
Louis <836250617@qq.com>
2024-02-08 12:15:29 +0800

    crate rust_version is wrong, rustc_version instead

--- fav_utils/build.rs
+++ fav_utils/build.rs
@@ -1,3 +1,5 @@
+use rustc_version::{version_meta, Channel};
+
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     // Set cfg flags depending on release channel
     let channel = match version_meta().unwrap().channel {
