289bf7f3e13b03b7d56842cac8ae60c87facc840
Louis <836250617@qq.com>
2024-02-09 14:51:37 +0800

    core: attr trait test

--- fav_core/src/attr.rs
+++ fav_core/src/attr.rs
@@ -3,9 +3,37 @@
 //! Helping to gain the `id`, `title` and so on for resource, resource set and upper.
 
 /// Attributes
+/// #Example
+/// ```
+/// # use fav_core::attr::Attr;
+///
+/// struct Video {
+///     id: String,
+///     name: String
+/// }
+///
+/// impl Attr for Video {
+///     fn id(&self) -> &str {
+///         &self.id
+///     }
+///
+///     fn name(&self) -> &str {
+///         &self.name
+///     }
+/// }
+///
+/// fn main() {
+///     let video = Video {
+///         id: "123123".to_string(),
+///         name: "name".to_string()
+///     };
+///
+///     assert_eq!(video.id().to_string(), "123123");
+///     assert_eq!(video.name(), "name");
+/// }
 pub trait Attr {
     /// Return the id of the target
-    fn id(&self) -> &str;
+    fn id(&self) -> &str; // Todo some id may be usize
     /// Return the name of the target
     fn name(&self) -> &str;
 }
@@ -18,3 +46,9 @@ pub trait ResSetAttr: Attr {}
 
 /// Attributes of a upper.
 pub trait UpperAttr: Attr {}
+
+#[cfg(test)]
+mod tests {
+    #[test]
+    fn attr_test() {}
+}
