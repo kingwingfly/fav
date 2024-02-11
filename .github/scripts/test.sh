b4a8bfae6def700ebbe7bc013f834c6dd2690c02
Louis <836250617@qq.com>
2024-02-11 22:55:11 +0800

    action: test when pr opened and synchronize

---
+++ .github/scripts/test.sh
@@ -0,0 +1,18 @@
+#! /bin/bash
+set -e
+
+# Statements waiting to be executed
+statements=(
+    "cargo fetch --locked"
+    "cargo clippy --all-features --all-targets -- -D warnings"
+    # fav_core
+    "cargo test -p fav_core"
+    "cargo doc --no-deps -p fav_core"
+)
+
+# loop echo and executing statements
+for statement in "${statements[@]}"; do
+    echo "$(tput setaf 3)$statement$(tput sgr0)"
+    eval $statement
+    echo
+done
