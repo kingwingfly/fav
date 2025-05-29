7344b8d515abbe5f69bcedd0e0c019bbdf7919d4
Louis <836250617@qq.com>
2025-05-29 11:31:44 +0800

    avoid .fav creation when generating completion scriptes

--- fav/src/command.rs
+++ fav/src/command.rs
@@ -105,90 +105,87 @@ impl FavCommand {
     }
 
     /// Parse the commands and args, return the Event to trigger.
-    pub fn run(self) -> Result<()> {
-        let matches = self.0.get_matches();
-
-        let mut world = World::new();
-        let mut schedule = Schedule::new(FavSchedule);
-        schedule.set_executor_kind(ExecutorKind::SingleThreaded);
-
-        let runtime = Runtime::new()?;
-        let db = runtime.block_on(Db::connect())?;
-        world.insert_resource(db);
-        world.insert_resource(runtime);
-
-        schedule.add_systems((
-            system::auth,
-            system::activate,
-            system::deactivate,
-            system::list,
-        ));
-        world.add_schedule(schedule);
-
-        world.run_schedule(FavSchedule);
+    pub fn run(mut self) -> Result<()> {
+        let matches = self.0.get_matches_mut();
 
         match matches.subcommand() {
-            Some(("auth", sub_matches)) => match sub_matches.subcommand() {
-                Some(("login", _)) => world.trigger(Login),
-                Some(("logout", sub_matches)) if sub_matches.get_flag("all") => {
-                    world.trigger(LogoutAll);
-                    // run again for events triggered by events
-                    world.run_schedule(FavSchedule);
-                }
-                Some(("logout", sub_matches)) => sub_matches
-                    .get_many::<i32>("account_id")
-                    .unwrap() // arg_required_else_help has been set to true
-                    .for_each(|&account_id| {
-                        world.trigger(Logout { account_id });
-                    }),
-                _ => unreachable!(),
-            },
-            Some(("list", sub_matches)) => match sub_matches.subcommand() {
-                Some(("account", _)) => world.trigger(ListUser),
-                Some(("set", _)) => todo!(),
-                Some(("up", _)) => todo!(),
-                Some(("video", _)) => todo!(),
-                _ => unreachable!(),
-            },
-            Some(("activate", sub_matches)) => match sub_matches.get_flag("all") {
-                true => {
-                    world.trigger(ActivateAll);
-                    // run again for events triggered by events
-                    world.run_schedule(FavSchedule);
-                }
-                false => sub_matches
-                    .get_many::<i32>("account_id")
-                    .unwrap() // arg_required_else_help has been set to true
-                    .for_each(|&account_id| {
-                        world.trigger(Activate { account_id });
-                    }),
-            },
-            Some(("deactivate", sub_matches)) => match sub_matches.get_flag("all") {
-                true => {
-                    world.trigger(DeactivateAll);
-                    // run again for events triggered by events
-                    world.run_schedule(FavSchedule);
-                }
-                false => sub_matches
-                    .get_many::<i32>("account_id")
-                    .unwrap() // arg_required_else_help has been set to true
-                    .for_each(|&account_id| {
-                        world.trigger(Deactivate { account_id });
-                    }),
-            },
             Some(("completion", sub_matches)) => {
-                let mut cmd = Self::new().0;
+                let bin_name = self.0.get_name().to_string();
                 let shell = *sub_matches.get_one::<Shell>("shell").unwrap();
-                clap_complete::generate(
-                    shell,
-                    &mut cmd,
-                    std::env::current_exe()
-                        .map(|path| path.file_name().unwrap().to_string_lossy().into_owned())
-                        .unwrap_or("fav".to_string()),
-                    &mut std::io::stdout(),
-                );
+                clap_complete::generate(shell, &mut self.0, bin_name, &mut std::io::stdout());
             }
-            _ => unreachable!(),
+            sub_cmd => {
+                let mut world = World::new();
+                let mut schedule = Schedule::new(FavSchedule);
+                schedule.set_executor_kind(ExecutorKind::SingleThreaded);
+
+                let runtime = Runtime::new()?;
+                let db = runtime.block_on(Db::connect())?;
+                world.insert_resource(db);
+                world.insert_resource(runtime);
+
+                schedule.add_systems((
+                    system::auth,
+                    system::activate,
+                    system::deactivate,
+                    system::list,
+                ));
+                world.add_schedule(schedule);
+
+                world.run_schedule(FavSchedule);
+
+                match sub_cmd {
+                    Some(("auth", sub_matches)) => match sub_matches.subcommand() {
+                        Some(("login", _)) => world.trigger(Login),
+                        Some(("logout", sub_matches)) if sub_matches.get_flag("all") => {
+                            world.trigger(LogoutAll);
+                            // run again for events triggered by events
+                            world.run_schedule(FavSchedule);
+                        }
+                        Some(("logout", sub_matches)) => sub_matches
+                            .get_many::<i32>("account_id")
+                            .unwrap() // arg_required_else_help has been set to true
+                            .for_each(|&account_id| {
+                                world.trigger(Logout { account_id });
+                            }),
+                        _ => unreachable!(),
+                    },
+                    Some(("list", sub_matches)) => match sub_matches.subcommand() {
+                        Some(("account", _)) => world.trigger(ListUser),
+                        Some(("set", _)) => todo!(),
+                        Some(("up", _)) => todo!(),
+                        Some(("video", _)) => todo!(),
+                        _ => unreachable!(),
+                    },
+                    Some(("activate", sub_matches)) => match sub_matches.get_flag("all") {
+                        true => {
+                            world.trigger(ActivateAll);
+                            // run again for events triggered by events
+                            world.run_schedule(FavSchedule);
+                        }
+                        false => sub_matches
+                            .get_many::<i32>("account_id")
+                            .unwrap() // arg_required_else_help has been set to true
+                            .for_each(|&account_id| {
+                                world.trigger(Activate { account_id });
+                            }),
+                    },
+                    Some(("deactivate", sub_matches)) => match sub_matches.get_flag("all") {
+                        true => {
+                            world.trigger(DeactivateAll);
+                            // run again for events triggered by events
+                            world.run_schedule(FavSchedule);
+                        }
+                        false => sub_matches
+                            .get_many::<i32>("account_id")
+                            .unwrap() // arg_required_else_help has been set to true
+                            .for_each(|&account_id| {
+                                world.trigger(Deactivate { account_id });
+                            }),
+                    },
+                    _ => unreachable!(),
+                }
+            }
         }
         Ok(())
     }
