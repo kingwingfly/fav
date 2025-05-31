94acdeb0d403d5699f3d01dd88e771da936b923d
Louis <836250617@qq.com>
2025-05-31 14:57:15 +0800

    restore list

---
+++ fav_bili/src/system/list.rs
@@ -0,0 +1,72 @@
+use bevy_ecs::{
+    observer::Trigger,
+    system::{Commands, Res},
+};
+use tracing::error;
+
+use crate::{
+    db::Db,
+    entity::ToTableRecord,
+    event::{ListAccount, ListMedia, ListSet, ListUp},
+    runtime::Runtime,
+    table::table,
+};
+
+pub fn list(mut cmds: Commands) {
+    cmds.add_observer(
+        |_: Trigger<ListAccount>, runtime: Res<Runtime>, db: Res<Db>| {
+            if let Err(e) = runtime.block_on(async {
+                let accounts = db.all_accounts().await?;
+                let table = table(
+                    ["account_id", "name", "state"],
+                    accounts.into_iter().map(ToTableRecord::to_record),
+                );
+                println!("{}\nrows: {}", table, table.count_rows() - 1);
+                Ok::<_, anyhow::Error>(())
+            }) {
+                error!("{}", e);
+            };
+        },
+    );
+    cmds.add_observer(|_: Trigger<ListSet>, runtime: Res<Runtime>, db: Res<Db>| {
+        if let Err(e) = runtime.block_on(async {
+            let sets = db.all_sets().await?;
+            let table = table(
+                ["set_id", "name", "count", "state"],
+                sets.into_iter().map(ToTableRecord::to_record),
+            );
+            println!("{}\nrows: {}", table, table.count_rows() - 1);
+            Ok::<_, anyhow::Error>(())
+        }) {
+            error!("{}", e);
+        };
+    });
+    cmds.add_observer(
+        |_: Trigger<ListMedia>, runtime: Res<Runtime>, db: Res<Db>| {
+            if let Err(e) = runtime.block_on(async {
+                let medias = db.all_medias().await?;
+                let table = table(
+                    ["id", "bvid", "title", "type", "state"],
+                    medias.into_iter().map(ToTableRecord::to_record),
+                );
+                println!("{}\nrows: {}", table, table.count_rows() - 1);
+                Ok::<_, anyhow::Error>(())
+            }) {
+                error!("{}", e);
+            };
+        },
+    );
+    cmds.add_observer(|_: Trigger<ListUp>, runtime: Res<Runtime>, db: Res<Db>| {
+        if let Err(e) = runtime.block_on(async {
+            let ups = db.all_ups().await?;
+            let table = table(
+                ["id", "name", "state"],
+                ups.into_iter().map(ToTableRecord::to_record),
+            );
+            println!("{}\nrows: {}", table, table.count_rows() - 1);
+            Ok::<_, anyhow::Error>(())
+        }) {
+            error!("{}", e);
+        };
+    });
+}
